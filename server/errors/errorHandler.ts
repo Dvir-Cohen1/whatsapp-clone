678f0483f3f74ede9db1485ed960a8dba97d37be
Dvir Cohen <dvir906@gmail.com>
2022-12-22 01:12:03 +0200

    improve error handler

--- server/errors/errorHandler.ts
+++ server/errors/errorHandler.ts
@@ -1,4 +1,9 @@
-import { NotFoundError, UnauthorizeError, BadRequestError } from "./Error";
+import {
+  NotFoundError,
+  UnauthorizeError,
+  BadRequestError,
+  ServerError,
+} from "./Error";
 
 /**
  * @param  {any} errorStack
@@ -8,32 +13,27 @@ function returnErrorStackByEnvironment(errorStack: any) {
   return process.env.NODE_ENV === "development" ? errorStack : {};
 }
 
+function generateCustomErrorResponse(res: any, error: any, statusCode: number) {
+  return res.status(statusCode).json({
+    error: true,
+    message: error.message,
+    stack: returnErrorStackByEnvironment(error.stack),
+  });
+}
+
 export function errorHandler(error: Error, req: any, res: any, next: any) {
   switch (error.constructor) {
     case NotFoundError:
-      return res.status(404).json({
-        error: true,
-        message: error.message,
-        stack: returnErrorStackByEnvironment(error.stack),
-      });
+      return generateCustomErrorResponse(res, error, 404);
 
     case UnauthorizeError:
-      // console.log(error.stack);
-      return res.status(403).json({
-        error: true,
-        message: error.message,
-        stack: returnErrorStackByEnvironment(error.stack),
-      });
+      return generateCustomErrorResponse(res, error, 403);
 
     case BadRequestError:
-      return res.status(400).json({
-        error: true,
-        message: error.message,
-        stack: returnErrorStackByEnvironment(error.stack),
-      });
+      return generateCustomErrorResponse(res, error, 400);
 
     default:
-      res.status(500).json({ ok: false, message: "Somthing Went Wrong!" });
+      res.status(500).json(new ServerError());
       break;
   }
 }
--- server/routes/authentication.routes.ts
+++ server/routes/authentication.routes.ts
@@ -1,11 +1,14 @@
 import express from "express";
-import { login, logout, register } from "../controllers/authentication.controllers";
+import {
+  login,
+  logout,
+  register,
+} from "../controllers/authentication.controllers";
 const router = express.Router();
-import { UnauthorizeError } from "../errors/Error";
 
 router.post("/register", register);
 router.post("/login", login);
-router.delete("/logout",logout);
+router.delete("/logout", logout);
 router.post("/refresh-token");
 router.post("/access-token");
 
